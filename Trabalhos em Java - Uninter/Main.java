136c1a3a17960cc41571a7c694b4f771d0d3d0fa
Willki Cavalcanti <68064056+Baraogit@users.noreply.github.com>
2021-07-10 23:57:29 -0300

    Update Main.java

--- Trabalhos em Java - Uninter/Main.java
+++ Trabalhos em Java - Uninter/Main.java
@@ -1,12 +1,5 @@
 import java.util.Scanner;
 
-import Entidades.Computador;
-import Entidades.Computador1;
-import Entidades.Computador2;
-import Entidades.Computador3;
-import Entidades.Jogador;
-import Entidades.Tabuleiro;
-
 public class Main {
 
 	public static void main(String[] args) {
@@ -20,13 +13,15 @@ public class Main {
 		Computador3 computador3 = new Computador3('X');
 	
 		char continuarJogo;
-		do {
+		do {//laço do-while que conterá a repetição do jogo
+			
 			// escolha do nível de difculdade
 			int nivelDificuldade;
 			do {
 				System.out.println("1 - Fácil");
 				System.out.println("2 - Moderado");
 				System.out.println("3 - Difícil");
+				
 				System.out.print("Escolha o nível de dificuldade do seu oponente: ");
 				nivelDificuldade = input.nextInt();
 				
@@ -43,21 +38,25 @@ public class Main {
 			System.out.println("01 | 02 | 03\n04 | 05 | 06\n07 | 08 | 09");
 			
 			
-			
 			int posicaoCampo;
 			boolean marcacaoCampo = false; 
-			//jogo em si
-			for(int i = 0; i < 5; i++) {
-				//jogador fará sua jogada escolhendo em qual posição irá jogar
+			int resultado;
 
-				do {
+			for(int i = 0; i < 5; i++) {
+				
+
+				do {//jogador fará sua jogada escolhendo em qual posição irá jogar
+					
 					System.out.print("Escolha a posição que você quer jogar: ");
 					posicaoCampo = input.nextInt();
+					
+					//se escolher uma posição fora do intervalo, retorna para o loop do-while
 					if(posicaoCampo < 1 || posicaoCampo > 9) {
 						System.out.println("Você digitou fora do intervalo. Tente novamente.\n\n");
-						continue;
+						continue;//retorna o loop para mais um tentativa
 					}
 					
+					// se escolher posição já estiver marcada, retorna o loop para mais um tentativa
 					marcacaoCampo = jogador.jogar(posicaoCampo, tabuleiro.campo);
 					if(marcacaoCampo == false) {
 						System.out.println("Posição já marcada. Tente outra.\n\n");
@@ -66,9 +65,9 @@ public class Main {
 
 				}while(marcacaoCampo == false || (posicaoCampo < 1 || posicaoCampo > 9));
 				
-				
 
-				/*Vez do computador. Se chegar na última rodada, o computador não joga, 
+				/*Vez do computador segundo a escolha do usuário. 
+				 * Na última rodada( i = 4), o computador não joga(pula a vez do computador), 
 				 pois sobra somente um campo vazio que será preenchida pelo jogador*/
 				if(i != 4) {
 					switch(nivelDificuldade) {
@@ -82,16 +81,12 @@ public class Main {
 						computador3.jogar(tabuleiro.campo);// nível de dificuldade 3
 						break;
 					}
-				}// fim do if
+				}
 		
+				tabuleiro.status();//mostra  status do tabuleiro no loop atual
 				
-				
-				tabuleiro.status();//mostra  status do tabuleiro
-				
-				
-				
-				//este bloco mostra o resultado do jogo
-				int resultado = tabuleiro.resultado();
+				//este bloco verifica se chegou a um resultado, se sim, mostra o vencedor
+				resultado = tabuleiro.resultado();
 				if(resultado == 1) {
 					System.out.println("Você venceu o computador!");
 					break;
@@ -104,10 +99,11 @@ public class Main {
 					System.out.println("Você empatou com o computador");
 					break;
 				}
-			}//fim do for
+				
+			}//fim do laço for 
 			
 			
-			
+			// pergunta se quer jogar novamente
 			while(true) {
 				System.out.print("Deseja jogar novamente?(s/n): ");
 				continuarJogo = input.next().charAt(0);
@@ -117,15 +113,17 @@ public class Main {
 					System.out.println("Ops! Escolha inválida, tente novamente.");
 					continue;
 				}	
-			}//fim while
+			}
 			
-			if(continuarJogo == 's')//zera os campos caso aja um novo jogo
+			//zera os campos caso decida jogar novamente
+			if(continuarJogo == 's')
 				tabuleiro.limparCampos();
 
 				
 		}while(continuarJogo != 'n');
 		
 	   input.close();
+	   
 	}//fim método main
 
 }// fim classe main
