33201988a0264e907e2f397bde66730fca73bf4f
Willki Cavalcanti <68064056+Baraogit@users.noreply.github.com>
2021-07-11 00:01:48 -0300

    Update Computador2.java

--- Trabalhos em Java - Uninter/Computador2.java
+++ Trabalhos em Java - Uninter/Computador2.java
@@ -9,18 +9,19 @@ public class Computador2 extends Computador {
 	public void jogar(char[][] campo) {
 	    boolean flag = false;
 	    
+	    
 		for(int i = campo.length - 1; i >= 0; i--) {
 			for(int j = campo.length - 1; j >= 0; j--) {
-				if(campo[i][j] != '_')
-				  continue;
+				if(campo[i][j] != '_') // se campo já estiver marcado
+				  continue;//pula a posição do campo atual
 				else{
-				    campo[i][j] = pecaTabuleiro;
+				    campo[i][j] = this.pecaTabuleiro;//se não, marca o campo com uma peça
 				    flag = true;
-				    break;
+				    break; //sai do loop interno
 				}
 			}
 			if(flag == true)
-			   break;
+			   break;//sai do loop externo
 		}
 	}//fim do método
 
